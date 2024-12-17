d0d4df2d72e89516b1d8b8031152026d297646f0
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2024-12-17 03:58:01 -0500

    tweak: remove static dimensions from home route, make play button green, simplify ui when VU not installed

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,8 @@
-import { getRandomNumber, playVU, vuIsInstalled } from '@/api'
+import { playVU, vuIsInstalled } from '@/api'
 import { Button } from '@/components/ui/button'
-import { DEFAULT_STALE_TIME, routes, STALE } from '@/config/config'
+import { routes, STALE } from '@/config/config'
 import { useQuery } from '@tanstack/react-query'
-import { Download, Loader, Play, Server } from 'lucide-react'
+import { Download, Loader, Play, Search, Server } from 'lucide-react'
 import { toast } from 'sonner'
 import { getCurrentWindow } from '@tauri-apps/api/window'
 import CredentialsSheet from '@/components/credentials-sheet'
@@ -39,23 +39,8 @@ export default function Home() {
   if (vuInstalled) {
     return (
       <div className="m-auto flex min-h-[100vh] flex-col justify-center">
-        <div className="m-auto flex h-[50vh] w-[50vw] flex-col justify-between gap-8 rounded-md bg-primary p-8">
-          <Button
-            variant={'secondary'}
-            className="p-8 text-4xl"
-            onClick={() => {
-              toast('Starting VU...')
-              playVU()
-              setTimeout(() => {
-                getCurrentWindow().minimize()
-              }, 1500)
-            }}
-          >
-            <Play />
-            <p>Play</p>
-          </Button>
-
-          <div className="flex flex-wrap gap-1 rounded-md bg-secondary p-2">
+        <div className="m-auto flex max-h-96 max-w-96 flex-col justify-between gap-8 rounded-md bg-primary p-8">
+          <div className="flex flex-1 flex-wrap gap-1 rounded-md bg-secondary p-2">
             <CredentialsSheet />
             <Link to={routes.SERVERS}>
               <Button>
@@ -68,23 +53,53 @@ export default function Home() {
               <Download />
             </Button>
           </div>
+
+          <Button
+            variant={'constructive'}
+            className="p-8 text-2xl"
+            onClick={() => {
+              toast('Starting VU...')
+              playVU()
+              setTimeout(() => {
+                getCurrentWindow().minimize()
+              }, 1500)
+            }}
+          >
+            <Play />
+            PLAY
+          </Button>
         </div>
       </div>
     )
   } else {
     return (
       <div className="m-auto flex min-h-[100vh] flex-col justify-center">
-        <div className="m-auto flex h-[50vh] w-[50vw] flex-col justify-center gap-8 rounded-md bg-primary p-8">
-          <Button
-            variant={'secondary'}
-            className="p-8 text-xl"
-            onClick={() => {
-              toast('Downloading VU...')
-              // downloadVU
-            }}
-          >
-            <Download size={'10px'} />
-            <p>Download VU</p>
+        <div className="m-auto flex max-h-96 max-w-96 flex-col justify-between gap-8 rounded-md bg-primary p-8">
+          <div className="flex flex-1 justify-center gap-4 align-middle text-xl leading-9 text-white">
+            <h1 className="flex-1">VU is installed?</h1>
+            <Button variant={'secondary'}>
+              <Search /> Find VU
+            </Button>
+          </div>
+
+          <div className="flex flex-1 justify-center gap-4 align-middle text-xl leading-9">
+            <h1 className="flex-1 text-white">VU not installed?</h1>
+            <Button
+              variant={'secondary'}
+              className=""
+              onClick={() => {
+                toast('Downloading VU...')
+                // downloadVU
+              }}
+            >
+              <Download size={'10px'} />
+              <p>Download VU</p>
+            </Button>
+          </div>
+
+          <Button variant={'destructive'} className="flex p-8 text-2xl" disabled>
+            <Play />
+            PLAY
           </Button>
         </div>
       </div>
