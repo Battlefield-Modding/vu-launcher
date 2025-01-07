5a4679526986483b9393b4e8fb4c5678f896b3fb
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-01-07 18:57:47 -0500

    fix(servers): handle overly long loadout names in delete menu

--- src/routes/Servers/components/DeleteLoadoutDialog.tsx
+++ src/routes/Servers/components/DeleteLoadoutDialog.tsx
@@ -36,7 +36,9 @@ function DeleteLoadoutDialog({ name }: { name: string }) {
         <DialogHeader>
           <DialogTitle className="pb-4">
             DELETE loadout:{' '}
-            <code className="text-md rounded-md bg-gray-800 p-1 pl-2 pr-2 text-white">{name}</code>?
+            <code className="text-md rounded-md bg-gray-800 p-1 pl-2 pr-2 text-white">
+              {name.length >= 20 ? `${name.substring(0, 20)}...` : name}
+            </code>
           </DialogTitle>
           <DialogDescription>
             This action cannot be undone. This will permanently delete all data related to this
@@ -52,7 +54,7 @@ function DeleteLoadoutDialog({ name }: { name: string }) {
           </DialogClose>
           <DialogClose onClick={handleDelete}>
             <p className="flex gap-4 rounded-md bg-red-600 p-2 text-white hover:bg-red-600/80">
-              <Trash /> Delete: {name}
+              <Trash /> Delete: {name.length >= 20 ? `${name.substring(0, 20)}...` : name}
             </p>
           </DialogClose>
         </div>
