0771c684c3915c780addaeee714408df6bbaee7f
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-03-17 17:55:07 -0400

    fix(onboarding): update vu dev shortcut in preferences

--- src-tauri/src/reg_functions.rs
+++ src-tauri/src/reg_functions.rs
@@ -2,7 +2,10 @@ use std::{ffi::OsString, io};
 
 use winreg::{enums::HKEY_CURRENT_USER, RegKey};
 
-use crate::{get_user_preferences_as_struct, update_vu_shortcut_preference};
+use crate::{
+    get_user_preferences_as_struct, update_vu_dev_shortcut_preference,
+    update_vu_shortcut_preference,
+};
 
 pub fn get_settings_json_path_registry() -> io::Result<String> {
     let install_path = get_reg_value("SettingsJson");
@@ -128,13 +131,13 @@ pub fn set_vu_dev_branch_install_location_registry(installdir: String) -> Result
     match set_key_result {
         Ok(_) => {
             println!("Set VU Key");
-            match update_vu_shortcut_preference() {
+            match update_vu_dev_shortcut_preference() {
                 true => {
                     return Ok(true);
                 }
                 false => {
-                    println!("Failed to update VU shortcut preference");
-                    return Err(String::from("Failed to update VU shortcut preference"));
+                    println!("Failed to update VU Dev shortcut preference");
+                    return Err(String::from("Failed to update VU Dev shortcut preference"));
                 }
             }
         }
