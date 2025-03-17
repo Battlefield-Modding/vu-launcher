b834a8ce50f0e3de73c113d084b3ff04a7a93178
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-03-17 15:16:52 -0400

    fix(onboarding): make loading text readable in new dark scheme

--- src/components/LoaderComponent.tsx
+++ src/components/LoaderComponent.tsx
@@ -2,7 +2,7 @@ import { Loader2Icon } from 'lucide-react'
 
 export function LoaderComponent() {
   return (
-    <div className="fixed bottom-0 left-0 flex min-h-[100vh] w-full flex-col bg-black bg-opacity-50 text-secondary">
+    <div className="fixed bottom-0 left-0 flex min-h-[100vh] w-full flex-col bg-black bg-opacity-50 text-primary">
       <div className="m-auto flex flex-col items-center">
         <p className="text-2xl">Loading... </p>
         <Loader2Icon className="h-16 w-16 animate-spin" />
