3dfbeba23dc11c9377948489ca6cbccdf491f9c8
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-03-28 05:11:04 -0400

    chore(locale): prettier eslint formatting

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,19 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { EnglishTranslations } from './translations/English';
-import { GermanTranslations } from './translations/German';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { EnglishTranslations } from './translations/English'
+import { GermanTranslations } from './translations/German'
 
 export enum i18nLanguageCodes {
-	Arabic = "ar",
-	Chinese = "zh",
-	English = "en",
-	Finnish = "fi",
-	French = "fr",
-	German = "de",
-	Spanish = "es",
-	Swedish = "sv",
-	Russian = "ru",
+  Arabic = 'ar',
+  Chinese = 'zh',
+  English = 'en',
+  Finnish = 'fi',
+  French = 'fr',
+  German = 'de',
+  Spanish = 'es',
+  Swedish = 'sv',
+  Russian = 'ru',
 }
 
 export const supportedLanguages = [i18nLanguageCodes.English, i18nLanguageCodes.German]
@@ -33,18 +33,18 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     resources: {
-			[i18nLanguageCodes.English]: {
+      [i18nLanguageCodes.English]: {
         translation: {
           // here we will place our translations...
-					...EnglishTranslations
-        }
+          ...EnglishTranslations,
+        },
       },
-			[i18nLanguageCodes.German]: {
-				translation: {
-					...GermanTranslations
-				}
-			}
-    }
-  });
+      [i18nLanguageCodes.German]: {
+        translation: {
+          ...GermanTranslations,
+        },
+      },
+    },
+  })
 
-export default i18n;
+export default i18n
