13337ce8895c43d60ffb31e38371366a8cad3985
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-04-03 18:02:29 -0400

    feat(locale): add createLoadoutSheet, deleteLoadoutDialog, and loadoutContainer to translation object

--- src/routes/Servers/components/Forms/CreateLoadout/CreateLoadoutSheet.tsx
+++ src/routes/Servers/components/Forms/CreateLoadout/CreateLoadoutSheet.tsx
@@ -5,9 +5,11 @@ import { CreateLoadoutForm } from './CreateLoadoutForm'
 import { QueryKey, STALE } from '@/config/config'
 import { useQuery } from '@tanstack/react-query'
 import { getModNamesInCache } from '@/api'
+import { useTranslation } from 'react-i18next'
 
 export function CreateLoadoutSheet() {
   const [sheetOpen, setSheetOpen] = useState(false)
+  const { t } = useTranslation()
 
   const { isPending, isError, data, error } = useQuery({
     queryKey: [QueryKey.GetModNamesInCache],
@@ -18,7 +20,7 @@ export function CreateLoadoutSheet() {
   if (isPending) {
     return (
       <div>
-        <h1>LOADING Mods</h1>
+        <h1>{t('servers.loadouts.createLoadout.sheet.loading')}</h1>
         <Loader />
       </div>
     )
@@ -27,7 +29,7 @@ export function CreateLoadoutSheet() {
   if (isError) {
     return (
       <div className="rounded-md bg-red-600 pl-2 pr-2 text-xl leading-9 text-white">
-        <h1>ERROR: No Mods Found</h1>
+        <h1>{t('servers.loadouts.createLoadout.sheet.error')}</h1>
         <p>{error.message}</p>
       </div>
     )
@@ -37,13 +39,15 @@ export function CreateLoadoutSheet() {
     <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
       <SheetTrigger>
         <div className="flex items-center justify-between gap-2 bg-green-700 p-2 text-lg text-primary hover:bg-green-700/80">
-          Create Loadout
+          {t('servers.loadouts.createLoadout.sheet.trigger')}
           <Plus />
         </div>
       </SheetTrigger>
       <SheetContent className="overflow-y-scroll">
         <SheetHeader>
-          <SheetTitle className="text-center text-2xl">Create Loadout</SheetTitle>
+          <SheetTitle className="text-center text-2xl">
+            {t('servers.loadouts.createLoadout.sheet.title')}
+          </SheetTitle>
         </SheetHeader>
         <br />
         <CreateLoadoutForm setSheetOpen={setSheetOpen} mods={data} />
