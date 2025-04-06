77917a732d6b1518c36aa397d0a0e1177874a79d
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-04-06 13:08:50 -0400

    fix(master): make importLoadout trigger full height

--- src/routes/Servers/components/Forms/UploadLoadout/UploadLoadoutSheet.tsx
+++ src/routes/Servers/components/Forms/UploadLoadout/UploadLoadoutSheet.tsx
@@ -45,7 +45,7 @@ export function UploadLoadoutSheet() {
   return (
     <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
       <SheetTrigger>
-        <div className="flex items-center justify-between gap-2 bg-gray-600 p-2 text-lg hover:bg-gray-600/80">
+        <div className="flex h-full items-center justify-between gap-2 bg-gray-600 p-2 text-lg hover:bg-gray-600/80">
           {t('servers.loadouts.importLoadout.sheet.trigger')}
           <Upload />
         </div>
