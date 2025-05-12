11e72470e741b3cd3217b8f1be64fd862947d023
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-05-12 13:27:24 -0400

    feat(master): add auto-update logic

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -10,12 +10,49 @@ import { firstTimeSetup, getUserPreferences, saveUserPreferences } from './api'
 import { invoke } from '@tauri-apps/api/core'
 import { Onboarding } from './components/Onboarding/Onboarding'
 
+import { check } from '@tauri-apps/plugin-updater'
+import { relaunch } from '@tauri-apps/plugin-process'
 export function AppLayout() {
   const [onboarding, setOnboarding] = useState(false)
   const { pathname } = useLocation()
   const navigate = useNavigate()
 
   useEffect(() => {
+    async function checkForUpdates() {
+      try {
+        const update = await check()
+        if (update) {
+          console.log(
+            `found update ${update.version} from ${update.date} with notes ${update.body}`,
+          )
+          let downloaded = 0
+          let contentLength = 0
+          // alternatively we could also call update.download() and update.install() separately
+          await update.downloadAndInstall((event) => {
+            switch (event.event) {
+              case 'Started':
+                // @ts-ignore
+                contentLength = event.data.contentLength
+                console.log(`started downloading ${event.data.contentLength} bytes`)
+                break
+              case 'Progress':
+                downloaded += event.data.chunkLength
+                console.log(`downloaded ${downloaded} from ${contentLength}`)
+                break
+              case 'Finished':
+                console.log('download finished')
+                break
+            }
+          })
+
+          console.log('update installed')
+          await relaunch()
+        }
+      } catch (err) {
+        console.log(`Failed to check for updates due to error:\n[${err}]`)
+      }
+    }
+
     async function handleOnboarding() {
       await firstTimeSetup()
       const preferences = await getUserPreferences()
@@ -36,6 +73,8 @@ export function AppLayout() {
     navigateToPreviousRoute()
 
     invoke('show_window')
+
+    checkForUpdates()
   }, [])
 
   useEffect(() => {
