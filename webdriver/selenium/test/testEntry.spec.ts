c778e0cedd24c864b9746f7cc26002b5af839dd2
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2025-06-27 09:50:25 -0400

    feat(tests): trash dev launcher on test start and add some startup tests

--- webdriver/selenium/test/testEntry.spec.ts
+++ webdriver/selenium/test/testEntry.spec.ts
@@ -2,8 +2,12 @@ import os from 'os'
 import path from 'path'
 import { Builder, Capabilities, WebDriver } from 'selenium-webdriver'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
-import { routes } from '@/config/config'
+import { routes, UserPreferences } from '@/config/config'
 import { spawn, spawnSync } from 'child_process'
+import { existsSync, readFileSync } from 'fs'
+import trash from 'trash'
+
+const localAppData = process.env.LOCALAPPDATA
 
 // create the path to the expected application binary
 const application = path.resolve(
@@ -24,7 +28,14 @@ let driver: WebDriver
 let tauriDriver: WebDriver
 
 beforeAll(async function () {
-  // unsure how to use the timeout from example
+  // Delete the entire vu-launcher-dev on start of test-suite
+  if (localAppData) {
+    const devInstallLocation = path.resolve(localAppData, 'vu-launcher-dev')
+    const folderExists = existsSync(devInstallLocation)
+    if (folderExists) {
+      await trash(devInstallLocation)
+    }
+  }
 
   // ensure the program has been built
   spawnSync('cargo', ['build', '--debug'])
@@ -55,12 +66,76 @@ afterAll(async function () {
   tauriDriver.kill()
 })
 
-describe('Hello World', () => {
+describe('First time startup', () => {
   const tauriURL = 'http://tauri.localhost'
   it('should start on onboarding page', async () => {
     // arrange
+    const targetURL = tauriURL + routes.ONBOARDING
     // act
     const currentRoute = await driver.getCurrentUrl()
-    expect(currentRoute).toEqual(tauriURL + routes.ONBOARDING)
+    // assert
+    expect(currentRoute).toEqual(targetURL)
+  })
+
+  it('should create folder appdata/local/vu-launcher-dev', async () => {
+    if (localAppData) {
+      // arrange
+      const devInstallLocation = path.resolve(localAppData, 'vu-launcher-dev')
+      // act
+      const status = existsSync(devInstallLocation)
+      // assert
+      expect(status).toBe(true)
+    } else {
+      expect(0).toEqual(1)
+    }
+  })
+
+  it('should create settings JSON file', async () => {
+    if (localAppData) {
+      // arrange
+      const settingsPath = path.resolve(localAppData, 'vu-launcher-dev', 'settings.json')
+      // act
+      const status = existsSync(settingsPath)
+      // assert
+      expect(status).toBe(true)
+    } else {
+      expect(0).toEqual(1)
+    }
+  })
+
+  it('should assign all settings in json file', async () => {
+    if (localAppData) {
+      // arrange
+      let jsonData: UserPreferences
+      const settingsPath = path.resolve(localAppData, 'vu-launcher-dev', 'settings.json')
+
+      // act
+      try {
+        const data = readFileSync(settingsPath, 'utf-8')
+        jsonData = JSON.parse(data)
+      } catch (e) {
+        console.error(e)
+        expect(0).toEqual(1)
+      }
+
+      // assert
+      // @ts-expect-error
+      if (jsonData) {
+        expect(jsonData.is_sidebar_enabled).toBeDefined()
+        expect(jsonData.venice_unleashed_shortcut_location).toBeDefined()
+        expect(jsonData.dev_venice_unleashed_shortcut_location).toBeDefined()
+        expect(jsonData.usernames).toBeDefined()
+        expect(jsonData.servers).toBeDefined()
+        expect(jsonData.server_guid).toBeDefined()
+        expect(jsonData.show_multiple_account_join).toBeDefined()
+        expect(jsonData.is_onboarded).toBeDefined()
+        expect(jsonData.use_dev_branch).toBeDefined()
+        expect(jsonData.preferred_player_index).toBeDefined()
+        expect(jsonData.preferred_server_index).toBeDefined()
+        expect(jsonData.last_visted_route).toBeDefined()
+        expect(jsonData.automatically_check_for_updates).toBeDefined()
+        expect(jsonData.automatically_install_update_if_found).toBeDefined()
+      }
+    }
   })
 })
